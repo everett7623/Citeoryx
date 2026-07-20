da012cef0d60cc53e6dc27eba93b17e016514af8
everett7623 <everett7623@gmail.com>
2026-07-21 00:08:19 +0800

    Add LICENSE and update plugin header license declaration

--- citeoryx.php
+++ citeoryx.php
@@ -5,7 +5,7 @@
  * Description: WordPress 内容健康度持续监控、优化与 AI 可发现性引擎
  * Version:     2.0.1
  * Author:      everettlabs
- * License:     GPL-2.0-or-later
+ * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: citeoryx
  * Domain Path: /languages
